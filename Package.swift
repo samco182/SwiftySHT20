498129dca0e2f9e742d3230ffa95eba29c8b7d2a
Samuel Cornejo <sam.ach1990@gmail.com>
2019-10-31 22:56:09 -0600

    [Lib] updates SwiftyGPIO library to use an updated branch

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", .exact("2.0.0-beta10")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
